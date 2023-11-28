3ad3950019ba7ea5ed197c3b1e5b2cd4b7a1883d
Nilesh Bhosale <nileshbhosale215@gmail.com>
2023-11-28 18:00:47 +0530

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -4,18 +4,6 @@ export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © Nilesh Bhosale.
-      // <a href="/feed.xml">RSS</a>
-      <style jsx>{`
-        a {
-          float: right;
-        }
-        @media screen and (max-width: 480px) {
-          article {
-            padding-top: 2rem;
-            padding-bottom: 4rem;
-          }
-        }
-      `}</style>
     </small>
   )
 }
