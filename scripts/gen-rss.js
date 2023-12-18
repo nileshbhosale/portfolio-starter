2c157c4176021cea24ce3cef8bc8d645cb604068
Nilesh Bhosale <nilesh@topmate.io>
2023-12-19 00:05:19 +0530

    updates

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Nilesh Bhosale',
+    site_url: 'https://www.nileshbhosale.com/blog',
+    feed_url: 'https://www.nileshbhosale.com/blog/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
